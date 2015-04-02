b2954845b1de5e3e3d1e181e4f08d6ab6c4db9c9
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2015-04-02 14:41:16 +0200

    feat(repository): implement add_files method

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use git::{};
-use repository::{};
+use repository::{FileData};
 
 mod git;
 mod repository;
 
 fn main() {
-    let r = git::init("foo").unwrap();
-    r.commit_all();
-    println!("Hello, world!");
+    let repository = git::init("foo").unwrap();
+
+    repository.add_files(vec!(
+        FileData { name: "foo.txt", content: "foobar" },
+        FileData { name: "bar.txt", content: "bar" },
+    ));
+
+    repository.commit_all();
+    println!("Look into the foo directory");
 }
--- src/repository.rs
+++ src/repository.rs
@@ -1,9 +1,18 @@
 use std::process::Command;
 use std::borrow::ToOwned;
 use std::path::Path;
+use std::fs::File;
+use std::io::Write;
 
+#[derive(Debug)]
 pub struct Repository<'a> {
     pub path: &'a str
+}
+
+#[derive(Debug)]
+pub struct FileData<'a> {
+    pub name: &'a str,
+    pub content: &'a str
 }
 
 impl<'a> Repository<'a> {
@@ -19,5 +28,15 @@ impl<'a> Repository<'a> {
         let buf = String::from_utf8_lossy(&output.stdout);
 
         buf.trim_matches('\n').to_owned()
+    }
+
+    pub fn add_files (&self, files: Vec<FileData>) {
+        files.iter().all(|file_data| {
+            let path = Path::new(self.path).join(file_data.name);
+            //FIXME: what's the best approach for error handling here?
+            let mut file = File::create(&path).ok().unwrap();
+            file.write(file_data.content.as_bytes()).ok();
+            true
+        });
     }
 }

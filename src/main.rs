677e4d4e33b49fb687179f9c5cb8ad801e85ce27
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2015-04-02 16:13:36 +0200

    feat(main): add basic param parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+extern crate clap;
+
+use clap::{App, Arg};
+//doesn't compile today, try tomorrow ;)
+//use uuid::Uuid;
 use git::{};
 use repository::{FileData};
 
@@ -5,6 +10,27 @@ mod git;
 mod repository;
 
 fn main() {
+
+    // Pull version from Cargo.toml
+    let version = format!("{}.{}.{}{}",
+                          env!("CARGO_PKG_VERSION_MAJOR"),
+                          env!("CARGO_PKG_VERSION_MINOR"),
+                          env!("CARGO_PKG_VERSION_PATCH"),
+                          option_env!("CARGO_PKG_VERSION_PRE").unwrap_or(""));
+
+    let matches = App::new("clog")
+        .version(&version[..])
+        .arg(Arg::new("init")
+            .short("i")
+            .long("init")
+            .help("if present, data should create new repository"))
+        .arg(Arg::new("data")
+            .short("d")
+            .long("data")
+            .help("e.g. { \"files\": { \"style.css\": \"button: { color: red; }\", \"app.js\": \"alert('foo');\" } }")
+            .takes_value(true))
+        .get_matches();
+
     let repository = git::init("foo").unwrap();
 
     repository.add_files(vec!(
